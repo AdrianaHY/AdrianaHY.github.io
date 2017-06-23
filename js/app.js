686c6e152e15dbb4599bfb6972e9cf0801385a52
AdrianaHY <adriana.herya@gmail.com>
2017-06-23 15:57:35 -0500

    ultimos arreglos

--- js/app.js
+++ js/app.js
@@ -4,15 +4,7 @@ var foto = document.getElementById("fotografia");
 var botonFoto = document.getElementById("botonFoto");
 
 
-// for (var i = 0; i < span.length; i++){
-//   span[i].addEventListener("click",cerrar);
-// }
-//
-//
-// function cerrar() {
-//   var div = this.parentElement;
-//   div.style.display = "none";
-// }
+
 
 botonFoto.addEventListener("click",abrirCerrar);
 
@@ -23,21 +15,3 @@ function abrirCerrar(){
     foto.style.display= "block";
   }
 }
-
-// botonDos.addEventListener("click", abrirCerrarParrafoDos);
-//
-// function abrirCerrarParrafoDos(){
-//   if(parrafoDos.style.visibility == "visible"){
-//     parrafoDos.style.visibility = "hidden";
-//   }else{
-//     parrafoDos.style.visibility = "visible";
-//   }
-// }
-//
-// botonRestaurar.addEventListener("click", restaurar);
-//
-// function restaurar(){
-//   for(var i=0; i < span.length; i++){
-//     span[i].parentElement.style.display= "inline";
-//   }
-// }
